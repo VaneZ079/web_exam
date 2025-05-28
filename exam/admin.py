ebffb28a10f4e2bc1da318d8266e7ecc7ebe3bf4
VaneZ079 <zligostev228@gmail.com>
2025-05-28 11:09:55 +0300

    настройка admin части для izexam

--- exam/admin.py
+++ exam/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Izexam
+from django.contrib.auth.models import User
 
-# Register your models here.
+@admin.register(Izexam)
+class IzexamAdmin(admin.ModelAdmin):
+    list_display = ('title', 'exam_date', 'create_date', 'is_public')
+    search_fields = ('title', 'write_users__email')
+    list_filter = ('is_public', 'create_date', 'exam_date')
+    filter_horizontal = ('write_users',)
